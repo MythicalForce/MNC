f0eeb14e81fb0a60d6cf84817a54e3cd38829171
Kristoffer Rekvik <62886250+MythicalForce@users.noreply.github.com>
2022-01-26 16:35:39 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 
 #include "controller.h"
 
+#include "pattern/solidcolor.h"
+
 //
 // Task Handles to our running threads
 //
@@ -27,6 +29,47 @@ TaskHandle_t g_taskSocket = nullptr;
   LEDController strand1(*strand1_cled);
 #endif
 
+#if NUM_CHANNELS >= 2
+  CRGB _leds1[Config[DEVICE].NUMLED[1]];
+  CLEDController *strand2_cled;
+  LEDController strand2(*strand2_cled);
+#endif
+
+#if NUM_CHANNELS >= 3
+  CRGB _leds2[Config[DEVICE].NUMLED[2]];
+  CLEDController *strand3_cled;
+  LEDController strand3(*strand3_cled);
+#endif
+
+#if NUM_CHANNELS >= 4
+  CRGB _leds3[Config[DEVICE].NUMLED[3]];
+  CLEDController *strand4_cled;
+  LEDController strand4(*strand4_cled);
+#endif
+
+#if NUM_CHANNELS >= 5
+  CRGB _leds4[Config[DEVICE].NUMLED[4]];
+  CLEDController *strand5_cled;
+  LEDController strand5(*strand5_cled);
+#endif
+
+#if NUM_CHANNELS >= 6
+  CRGB _leds5[Config[DEVICE].NUMLED[5]];
+  CLEDController *strand6_cled;
+  LEDController strand6(*strand6_cled);
+#endif
+
+#if NUM_CHANNELS >= 7
+  CRGB _leds6[Config[DEVICE].NUMLED[6]];
+  CLEDController *strand7_cled;
+  LEDController strand7(*strand7_cled);
+#endif
+
+#if NUM_CHANNELS >= 8
+  CRGB _leds7[Config[DEVICE].NUMLED[7]];
+  CLEDController *strand8_cled;
+  LEDController strand8(*strand8_cled);
+#endif
 void IRAM_ATTR SocketServerTaskEntry(void *)
 {
     for (;;)
@@ -71,48 +114,42 @@ void setup()
   debug.text("------------------------------------------------------------------------------------- \n");
   #if NUM_CHANNELS >= 1
     strand1_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds0, Config[DEVICE].NUMLED[0] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 1 ACTIVE on pin %d with %d/%d LEDs", LED_PIN1, Config[DEVICE].NUMLED[0], LED_AMOUNT_PER_PIN);
   #endif
 
   #if NUM_CHANNELS >= 2
-    CRGB _leds1[Config[DEVICE].NUMLED[1]];
-    CLEDController *strand2_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds1, Config[DEVICE].NUMLED[1] ).setCorrection( TypicalLEDStrip );
-    LEDController strand2(*strand2_cled);
+    strand2_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds1, Config[DEVICE].NUMLED[1] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 2 ACTIVE on pin %d with %d/%d LEDs", LED_PIN2, Config[DEVICE].NUMLED[1], LED_AMOUNT_PER_PIN);
   #endif
 
   #if NUM_CHANNELS >= 3
-    CRGB _leds2[Config[DEVICE].NUMLED[2]];
-    CLEDController *strand3_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds2, Config[DEVICE].NUMLED[2] ).setCorrection( TypicalLEDStrip );
-    LEDController strand3(*strand3_cled);
+    strand3_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds2, Config[DEVICE].NUMLED[2] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 3 ACTIVE on pin %d with %d/%d LEDs", LED_PIN3, Config[DEVICE].NUMLED[2], LED_AMOUNT_PER_PIN);
   #endif
 
   #if NUM_CHANNELS >= 4
-    CRGB _leds3[Config[DEVICE].NUMLED[3]];
-    CLEDController *strand4_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds3, Config[DEVICE].NUMLED[3] ).setCorrection( TypicalLEDStrip );
-    LEDController strand4(*strand4_cled);
+    strand4_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds3, Config[DEVICE].NUMLED[3] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 4 ACTIVE on pin %d with %d/%d LEDs", LED_PIN4, Config[DEVICE].NUMLED[3], LED_AMOUNT_PER_PIN);
   #endif
 
   #if NUM_CHANNELS >= 5
-    CRGB _leds4[Config[DEVICE].NUMLED[4]];
-    CLEDController *strand5_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds4, Config[DEVICE].NUMLED[4] ).setCorrection( TypicalLEDStrip );
-    LEDController strand5(*strand5_cled);
+    strand5_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds4, Config[DEVICE].NUMLED[4] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 5 ACTIVE on pin %d with %d/%d LEDs", LED_PIN5, Config[DEVICE].NUMLED[4], LED_AMOUNT_PER_PIN);
   #endif
 
   #if NUM_CHANNELS >= 6
-    CRGB _leds5[Config[DEVICE].NUMLED[5]];
-    CLEDController *strand6_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds5, Config[DEVICE].NUMLED[5] ).setCorrection( TypicalLEDStrip );
-    LEDController strand6(*strand6_cled);
+    strand6_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds5, Config[DEVICE].NUMLED[5] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 6 ACTIVE on pin %d with %d/%d LEDs", LED_PIN6, Config[DEVICE].NUMLED[5], LED_AMOUNT_PER_PIN);
   #endif
 
   #if NUM_CHANNELS >= 7
-    CRGB _leds6[Config[DEVICE].NUMLED[6]];
-    CLEDController *strand7_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds6, Config[DEVICE].NUMLED[6] ).setCorrection( TypicalLEDStrip );
-    LEDController strand7(*strand7_cled);
+    strand7_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds6, Config[DEVICE].NUMLED[6] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 7 ACTIVE on pin %d with %d/%d LEDs", LED_PIN7, Config[DEVICE].NUMLED[6], LED_AMOUNT_PER_PIN);
   #endif
 
   #if NUM_CHANNELS >= 8
-    CRGB _leds7[Config[DEVICE].NUMLED[7]];
-    CLEDController *strand8_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds7, Config[DEVICE].NUMLED[7] ).setCorrection( TypicalLEDStrip );
-    LEDController strand8(*strand8_cled);
+    strand8_cled = &FastLED.addLeds<WS2812B, LED_PIN1, GRB>( _leds7, Config[DEVICE].NUMLED[7] ).setCorrection( TypicalLEDStrip );
+    debug.info("[FASTLED]", "[SETUP]", "Strip 8 ACTIVE on pin %d with %d/%d LEDs", LED_PIN8, Config[DEVICE].NUMLED[7], LED_AMOUNT_PER_PIN);
   #endif
   debug.newline();
 
