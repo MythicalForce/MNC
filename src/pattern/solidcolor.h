77925b23200b39b0ad02d2869b37f756bb21aa48
Kristoffer Rekvik <62886250+MythicalForce@users.noreply.github.com>
2022-01-26 16:40:37 +0100

    Add files via upload

--- src/pattern/solidcolor.h
+++ src/pattern/solidcolor.h
@@ -1,9 +1,22 @@
 #include "pattern.h"
+#include "draw.h"
 
 class Solidcolor : public Pattern
 {
-	virtual void Draw()
+	virtual void draw(CLEDController& c, const Palette&)
 	{
-        //fadeToBlackBy(_Leds.data(), _NumLeds, 255);
+        CRGB* leds = c.leds();
+        int size = c.size();
+        fadeToBlackBy(leds, size, 255);
+
+        for(float i = 0; i < size; i++)
+        {   
+            DrawPixels(i, 1, PaletteMode(c, i));
+        }
+    }
+
+	virtual void update()
+	{
+        
     }
 };
